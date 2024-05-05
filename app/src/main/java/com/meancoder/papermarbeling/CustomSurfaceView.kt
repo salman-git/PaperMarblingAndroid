69f9c7050f21e8f0fa3aa5ab22963b4c60aa5051
salman-git <ahmadsalman145@gmail.com>
2024-05-06 00:28:12 +0500

    menus added (not styled) but working. horizontal vertical comb and PEN + drawing mode and tine mode implemented.

--- app/src/main/java/com/meancoder/papermarbeling/CustomSurfaceView.kt
+++ app/src/main/java/com/meancoder/papermarbeling/CustomSurfaceView.kt
@@ -6,29 +6,86 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.PointF
 import android.util.AttributeSet
-import android.util.Log
 import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
-import android.widget.Toast
 import kotlin.random.Random
 
 class CustomSurfaceView : SurfaceView, SurfaceHolder.Callback {
+    enum class DrawingMode {
+        MODE_NORMAL,
+        MODE_TINE,
+        MODE_RANDOM_COLOR
+    }
+    enum class PEN_TYPE {
+        COMB_HORIZONTAL,
+        COMB_VERTICAL,
+        PEN
+    }
+
+    private lateinit var firstPoint: PointF
+
+    // Variable to store the current drawing mode
+    private var currentDrawingMode = DrawingMode.MODE_NORMAL
+    private var currentCombType: PEN_TYPE? = PEN_TYPE.PEN
     private val TAG: String = CustomSurfaceView::javaClass.javaClass.simpleName
     private var drawingThread: DrawingThread? = null
     private var drops: MutableList<Drop>? = null
+    private var lastX:Float? = null
+    private var lastY:Float? = null
     private var palette: MutableList<Int> = mutableListOf(
-        Color.rgb(11, 106, 136),
-        Color.rgb(45, 197, 244),
-        Color.rgb(112, 50, 126),
-        Color.rgb(146, 83, 161),
-        Color.rgb(164, 41, 99),
-        Color.rgb(236, 1, 90),
-        Color.rgb(240, 99, 164),
-        Color.rgb(241, 97, 100),
-        Color.rgb(248, 158, 79)
+        Color.rgb(255, 0, 0),
+        Color.rgb(0, 255, 0),
+        Color.rgb(0, 0, 255),
+        Color.rgb(255, 255, 0),
+        Color.rgb(255, 0, 255),
+        Color.rgb(0, 255, 255),
+        Color.rgb(128, 0, 0),
+        Color.rgb(0, 128, 0),
+        Color.rgb(0, 0, 128),
+        Color.rgb(128, 128, 0),
+        Color.rgb(128, 0, 128),
+        Color.rgb(0, 128, 128),
+        Color.rgb(255, 128, 0),
+        Color.rgb(0, 255, 128),
+        Color.rgb(128, 255, 0),
+        Color.rgb(0, 128, 255),
+        Color.rgb(255, 0, 128),
+        Color.rgb(128, 0, 255),
+        Color.rgb(255, 128, 128),
+        Color.rgb(128, 255, 128),
+        Color.rgb(128, 128, 255),
+        Color.rgb(255, 255, 128),
+        Color.rgb(255, 128, 255),
+        Color.rgb(128, 255, 255),
+        Color.rgb(192, 0, 0),
+        Color.rgb(0, 192, 0),
+        Color.rgb(0, 0, 192),
+        Color.rgb(192, 192, 0),
+        Color.rgb(192, 0, 192),
+        Color.rgb(0, 192, 192),
+        Color.rgb(192, 192, 192),
+        Color.rgb(128, 128, 128),
+        Color.rgb(64, 64, 64),
+        Color.rgb(255, 255, 255),
+        Color.rgb(255, 0, 0),
+        Color.rgb(0, 255, 0),
+        Color.rgb(0, 0, 255),
+        Color.rgb(255, 255, 0),
+        Color.rgb(255, 0, 255),
+        Color.rgb(0, 255, 255),
+        Color.rgb(128, 0, 0),
+        Color.rgb(0, 128, 0),
+        Color.rgb(0, 0, 128),
+        Color.rgb(128, 128, 0),
+        Color.rgb(128, 0, 128),
+        Color.rgb(0, 128, 128),
+        Color.rgb(255, 128, 0),
+        Color.rgb(0, 255, 128),
+        Color.rgb(128, 255, 0),
+        Color.rgb(0, 128, 255)
     )
-    private var bk: Int = Color.rgb(252, 238, 33)
+    private var bk: Int = Color.rgb(252, 33, 192)
 
     constructor(context: Context?) : super(context) {
         init()
@@ -71,21 +128,50 @@ class CustomSurfaceView : SurfaceView, SurfaceHolder.Callback {
         val y = event.y
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                val paint = Paint().apply {
-                    color = getRandomColor()
-                    isAntiAlias = true
-                    style = Paint.Style.FILL
+                firstPoint = PointF(x, y)
+                lastX = x
+                lastY = y
+                if(currentDrawingMode == DrawingMode.MODE_NORMAL) {
+                    val paint = Paint().apply {
+                        color = getRandomColor()
+                        isAntiAlias = true
+                        style = Paint.Style.FILL
+                    }
+                    dropThread = DropThread(x, y, paint) { x, y, r, paint ->
+                        addInk(x, y, r, paint)
+                    }
+                    dropThread.start()
                 }
-                dropThread = DropThread(x, y, paint) { x, y, r, paint ->
-                    addInk(x, y, r, paint)
-                }
-                dropThread.start()
             }
             MotionEvent.ACTION_UP -> {
                 dropThread.stopThread()
             }
             MotionEvent.ACTION_MOVE -> {
-                dropThread.updateXY(x, y)
+                if(currentDrawingMode == DrawingMode.MODE_NORMAL)
+                    dropThread.updateXY(x, y)
+
+                if (currentDrawingMode == DrawingMode.MODE_TINE) {
+                    val X = if(Math.abs(firstPoint.x - x) < 50) firstPoint.x else x
+                    val Y = if(Math.abs(firstPoint.y - y) < 50) firstPoint.y else y
+
+                    val dx = X - lastX!!
+                    val dy = Y - lastY!!
+                    val mag = Math.sqrt(dx.toDouble() * dx + dy * dy)
+                    if (mag > 0.1) {
+                        val vector = PointF((dx / mag).toFloat(), (dy / mag).toFloat())
+                        if (currentCombType == PEN_TYPE.COMB_VERTICAL) {
+                            for (Xc in 0..width step 100)
+                                tineLine(vector, Xc.toFloat(), 0f, 4f, 32f)
+                        }else if (currentCombType == PEN_TYPE.COMB_HORIZONTAL) {
+                            for (Yc in 0..height step 100)
+                                tineLine(vector, 0f, Yc.toFloat(), 4f, 32f)
+                        } else if (currentCombType == PEN_TYPE.PEN){
+                            tineLine(vector, X, Y, 4f, 32f)
+                        }
+                    }
+                    lastX = X
+                    lastY = Y
+                }
             }
         }
 
@@ -98,6 +184,12 @@ class CustomSurfaceView : SurfaceView, SurfaceHolder.Callback {
             other.marble(drop)
         }
         drops!!.add(drop)
+    }
+
+    fun tineLine(m: PointF, x: Float, y: Float, z: Float, c: Float) {
+        for (other in drops!!) {
+            other.tine(m, x, y, z, c)
+        }
     }
 
     override fun draw(canvas: Canvas) {
@@ -135,6 +227,26 @@ class CustomSurfaceView : SurfaceView, SurfaceHolder.Callback {
             radiusMultiplier = 1f
             isRunning = false
         }
+    }
+
+    fun toggleDrawingMode() {
+        currentDrawingMode = if (currentDrawingMode == DrawingMode.MODE_NORMAL) {
+            DrawingMode.MODE_TINE
+        } else {
+            DrawingMode.MODE_NORMAL
+        }
+    }
+
+    fun getDropsSize(): Int {
+        return drops!!.size
+    }
+
+    fun clear() {
+        drops?.clear()
+    }
+
+    fun setComb(comb: PEN_TYPE) {
+        currentCombType = comb
     }
 
 }
--- app/src/main/java/com/meancoder/papermarbeling/MainActivity.kt
+++ app/src/main/java/com/meancoder/papermarbeling/MainActivity.kt
@@ -1,23 +1,54 @@
 package com.meancoder.papermarbeling
 
 import android.os.Bundle
-import android.webkit.WebView
+import android.util.Log
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.meancoder.papermarbeling.ui.theme.PaperMarbelingTheme
 
 class MainActivity : ComponentActivity() {
+    private val TAG: String = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContent {
             PaperMarbelingTheme {
                 // A surface container using the 'background' color from the theme
@@ -25,31 +56,110 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    WebView(LocalContext.current).apply{
-//                        loadUrl("file:///android_asset/index.html")
-//                    }
-                    CustomSurfaceViewWrapper()
+                    val customSurfaceView by remember {
+                        mutableStateOf(CustomSurfaceView(this)) }
+                    var isLeftMenuVisible by remember { mutableStateOf(true) }
+                    var isRightMenuVisible by remember { mutableStateOf(true) }
+
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        AndroidView(factory = { context ->
+                            customSurfaceView
+                        })
+                        Button(
+                            onClick = { isRightMenuVisible = !isRightMenuVisible},
+                            modifier = Modifier.align(Alignment.TopEnd)
+                        ) {
+                            Text(text = "Hide Right Menu")
+                        }
+
+                        // Left menu Hide button
+                        Button(
+                            onClick = { isLeftMenuVisible = !isLeftMenuVisible },
+                            modifier = Modifier.align(Alignment.TopStart)
+                        ) {
+                            Text(text = "Hide Left Menu")
+                        }
+
+                        AnimatedVisibility(
+                            visible = isLeftMenuVisible,
+                            enter = slideInHorizontally(),
+                            exit = slideOutHorizontally(),
+                            modifier = Modifier.align(Alignment.CenterStart)
+                        ) {
+                                LeftMenu(customSurfaceView)
+                        }
+
+                        // Right menu
+                        AnimatedVisibility(
+                            visible = isRightMenuVisible,
+                            enter = slideInHorizontally(initialOffsetX = { it }),
+                            exit = slideOutHorizontally(targetOffsetX = { it }),
+                            modifier = Modifier.align(Alignment.CenterEnd)
+                        ) {
+                                RightMenu(customSurfaceView)
+                        }
+                    }
                 }
             }
         }
     }
-}
-@Composable
-fun CustomSurfaceViewWrapper() {
-    AndroidView(factory={context -> CustomSurfaceView(context)})
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PaperMarbelingTheme {
-        Greeting("Android")
+    @Composable
+    fun RightMenu(customSurfaceView: CustomSurfaceView) {
+        Log.i(TAG, "right menu created ${customSurfaceView.getDropsSize()}")
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Button(
+                    onClick = {
+                        customSurfaceView.toggleDrawingMode()
+                    },
+                    modifier = Modifier.padding(bottom = 8.dp) // Add padding between buttons
+                ) {
+                    Text(text = "Toggle Mode")
+                }
+                Button(
+                    onClick = {
+                        customSurfaceView.clear()
+                    }
+                ) {
+                    Text(text = "Clear")
+                }
+            }
+
+    }
+
+    @Composable
+    fun LeftMenu(customSurfaceView:CustomSurfaceView) {
+
+            Column(
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Button(
+                    onClick = {
+                        customSurfaceView.setComb(CustomSurfaceView.PEN_TYPE.COMB_HORIZONTAL)
+                    }
+                ) {
+                    Text(text = "HC") //horizontal comb
+                }
+                Button(
+                    onClick = {
+                        customSurfaceView.setComb(CustomSurfaceView.PEN_TYPE.COMB_VERTICAL)
+                    }
+                ) {
+                    Text(text = "YC") //vertical comb
+                }
+                Button(
+                    onClick = {
+                        customSurfaceView.setComb(CustomSurfaceView.PEN_TYPE.PEN)
+                    }
+                ) {
+                    Text(text = "Pen")
+                }
+            }
+
     }
 }
+
+
